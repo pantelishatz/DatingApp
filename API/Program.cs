e92d650abd43deb8e261e2d62fe21bb2bdb3807b
pantelishatz <xapantelis@hotmail.com>
2023-12-05 23:26:14 +0200

    Angular client

--- API/Program.cs
+++ API/Program.cs
@@ -11,15 +11,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
