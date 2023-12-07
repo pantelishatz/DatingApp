75a748b78639efc653e60d9478fdd5efb7a94d7b
pantelishatz <xapantelis@hotmail.com>
2023-12-07 22:24:51 +0200

    Nav Component Client Login

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,17 +4,19 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,29 @@
+import { Component , OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+  loggedIn = false;
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+      
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: error => console.log(error)
+    })
+  }
+
+}
