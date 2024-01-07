17d62c29318d8e61a7c2e29ffa01eda5fc52944c
pantelishatz <xapantelis@hotmail.com>
2024-01-07 20:57:10 +0200

    SeedData and UserRepository

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -16,6 +16,7 @@ namespace API.Extensions
             });
             services.AddCors();
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
 
             return services;
         }
---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByUsernameAsync(string username);
+
+    }
+}
