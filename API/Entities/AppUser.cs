6ad7c6cd2e07cace49c2a873425562f709dba873
pantelishatz <xapantelis@hotmail.com>
2024-05-07 22:54:37 +0300

    LikeUser feature (migration,Repository,Dto)

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -19,11 +19,8 @@ public class AppUser
     public string City { get; set; }
     public string Country { get; set; }
     public List<Photo> Photos { get; set; } = new();
-
-    // public int GetAge()
-    //  {
-    //     return DateOfBirth.CalculateAge();
-    // }
+    public List<UserLike> LikedByUsers { get; set; }
+    public List<UserLike> LikedUsers { get; set; }
 
 
 
