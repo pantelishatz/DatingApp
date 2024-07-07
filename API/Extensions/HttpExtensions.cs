9febb4e9c2f08c438b91671bb1256e34c099ef41
pantelishatz <xapantelis@hotmail.com>
2024-07-07 13:48:28 +0300

    Update in .NET8

--- API/Extensions/HttpExtensions.cs
+++ API/Extensions/HttpExtensions.cs
@@ -8,8 +8,8 @@ namespace API.Extensions
         public static void AddPaginationHeader(this HttpResponse response, PaginationHeader header)
         {
             var jsonOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
-            response.Headers.Add("Pagination", JsonSerializer.Serialize(header, jsonOptions));
-            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+            response.Headers.Append("Pagination", JsonSerializer.Serialize(header, jsonOptions));
+            response.Headers.Append("Access-Control-Expose-Headers", "Pagination");
         }
     }
 
