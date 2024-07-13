b0a6ba3f4b98e360dece1475b815769af05c4c58
pantelishatz <xapantelis@hotmail.com>
2024-07-13 22:25:47 +0300

    Updating to Angular17

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,18 +9,18 @@ public class AppUser
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
     public DateOnly DateOfBirth { get; set; }
-    public string KnownAs { get; set; }
+    public required string KnownAs { get; set; }
     public DateTime Created { get; set; } = DateTime.UtcNow;
     public DateTime LastActive { get; set; } = DateTime.UtcNow;
-    public string Gender { get; set; }
+    public required string Gender { get; set; }
     public string Introduction { get; set; }
     public string LookingFor { get; set; }
     public string Interests { get; set; }
     public string City { get; set; }
     public string Country { get; set; }
-    public List<Photo> Photos { get; set; } = new();
-    public List<UserLike> LikedByUsers { get; set; }
-    public List<UserLike> LikedUsers { get; set; }
+    public List<Photo> Photos { get; set; } = [];
+    public List<UserLike> LikedByUsers { get; set; } = [];
+    public List<UserLike> LikedUsers { get; set; } = [];
 
 
 
